77ef5b49a95a8ace8be1f7f238ce1afeec18302e
TechDanny <kokidanny15@gmail.com>
2023-03-16 07:36:21 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 /*
  * file name: 4-puts.c
- * author: dannykoki15@gmail.com
+ * author: kokidanny15@gmail.com
  */
 #include<stdio.h>
+
 /**
- * main-format "Programming is like building a multilingual puzzle
- *
- * return: always 0
+ * main-format "Programming is like building a multilingual puzzle.
+ * return: always 0.
  */
 int main(void)
 {
-	puts("\""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
