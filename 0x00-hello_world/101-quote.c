04c12ca54ecc7e25dc8bb17cbdb184700d1d0cd3
TechDanny <kokidanny15@gmail.com>
2023-03-16 16:43:45 -0700

    qute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,13 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-	return (1);
+#include <unistd.h>
+
+int main(void) {
+  const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  const int len = sizeof(msg) - 1;
+  const int ret = 1;
+  write(2, msg, len);
+  return ret;
 }
+
