259b084a2d08b88cc0ebbfa33c80b20fd107af0c
TechDanny <kokidanny15@gmail.com>
2023-03-16 06:09:29 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-int main(){puts("Programming is like building a multilingual puzzle\n"); return 0; }
+#include<stdio.h>  int main() { char Mystr[] = "with proper grammar, but the outcome is a piece of art"; puts(Mystr); return 0; }
