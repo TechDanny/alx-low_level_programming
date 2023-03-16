cd83fd8bf36fae9586c9f4b481d07f4ffd280677
TechDanny <kokidanny15@gmail.com>
2023-03-16 07:00:17 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,8 @@
  *    * Return: Always 0 (Success)
  *     */
 int main(void)
-{           puts("Programming is like building a multilingual puzzle");
-	
-	    return (0);
+{
+	int Mystr[] = "Programming is like building a multilingual puzzle;
+	puts(Mystr);
+	return 0;
 }
