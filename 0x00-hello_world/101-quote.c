21bdd4f68eb8ad9ecd79803fe96500472d629bad
TechDanny <kokidanny15@gmail.com>
2023-03-16 16:48:55 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,12 @@
  */
 #include <unistd.h>
 
-int main(void) {
-  const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  const int len = sizeof(msg) - 1;
-  const int ret = 1;
-  write(2, msg, len);
-  return ret;
+int main(void) 
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+       	const int len = sizeof(msg) - 1;
+	const int ret = 1;
+      	write(2, msg, len);
+	return ret();
 }
 
