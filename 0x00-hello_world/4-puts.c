51515a2d1fdf2d19e96b463b5e08e9d38d35f131
TechDanny <kokidanny15@gmail.com>
2023-03-16 07:14:53 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	int Mystr[] = "Programming is like building a multilingual puzzle;
-	puts(Mystr);
+	puts("Programming is like building a multilingual puzzle);
 	return 0;
 }
