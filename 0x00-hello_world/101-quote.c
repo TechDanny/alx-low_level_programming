8242bd35a1d6a80d027d20331088b7afea6676b5
TechDanny <kokidanny15@gmail.com>
2023-03-16 18:35:42 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 	char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, msg, 59);	/* this is a blank space */
+	write(STDERR_FILENO, msg, 59);			/* this is a blank space */
 	return (1);
 }
 
