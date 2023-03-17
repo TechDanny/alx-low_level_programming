a159e2e9ce30efd568eea2e6e3d4d37878a25718
TechDanny <kokidanny15@gmail.com>
2023-03-17 14:08:09 -0700

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,19 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	int i;
 
-	putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar('\n');
+	}
 	return (0);
 }
