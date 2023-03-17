c572fadc224c7a1dc6c968760d2a315d2563c56b
TechDanny <kokidanny15@gmail.com>
2023-03-16 17:59:05 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include<stdio.h>
-#include<stdio.h>
+#include<stdlib.h>
+
 /**
  * main - Entry point
  *
  * Return: Always  (Success)
  */
-#include <unistd.h>
-
 int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 	return (1);
 }
 
