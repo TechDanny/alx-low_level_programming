e69904b593f63c4db59e2df25821a57b9702f287
TechDanny <kokidanny15@gmail.com>
2023-03-16 17:47:27 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always  (Success)
  */
 #include <unistd.h>
 
 int main(void)
 {
-	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	const int len = sizeof(msg) - 1;
-	write(2, msg, len);
-	
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
 }
+
 
