c4c5743c3ec9bacc7bd725dc6d2e74073d3fdc28
TechDanny <kokidanny15@gmail.com>
2023-03-16 17:18:58 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,10 @@ int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	const int len = sizeof(msg) - 1;
+
 	write(2, msg, len);
 
+	
 	return (1);
 }
 
