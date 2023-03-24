58a9bb17155365a6bbb01356eb7cdba36d5cc5c2
TechDanny <kokidanny15@gmail.com>
2023-03-24 02:22:48 -0700

    100 prime factors

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,12 +1,39 @@
 #include<stdio.h>
+#include<math.h>
 
 /**
- * Main - entry point
+ * main - entry point
  *
- * Return: always 0(success)
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
+	long long n = 612852475143;
+	long long largest_factor = 1;
+	long long i;
+
+	while (n % 2 == 0)
+	{
+	largest_factor = 2;
+	n /= 2;
+	}
+
+	for (i = 3; i * i <= n; i += 2)
+	{
+	while (n % i == 0)
+	{
+	largest_factor = i;
+	n /= i;
+	}
+	}
+
+	if (n > 2)
+	{
+	largest_factor = n;
+	}
+
+	printf("%lld\n", largest_factor);
+
 	return (0);
 }
---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: charcter to be printed
+ *
+ * return: n to be returned
+ */
+
+void print_number(int n)
+{
+	return (0);
+}
