f562ebf06ecf289841234b894e97291cd26a9ac0
TechDanny <kokidanny15@gmail.com>
2023-03-24 02:34:17 -0700

    print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,5 +9,22 @@
 
 void print_number(int n)
 {
-	return (0);
+	if (n < 0)
+	{
+	_putchar('-');
+	n = -n;
+	}
+
+	int divisor = 1;
+
+	while (divisor <= n / 10)
+	{
+	divisor *= 10;
+	}
+
+	while (divisor > 0)
+	{
+	_putchar((n / divisor) % 10 + '0');
+	divisor /= 10;
+	}
 }
