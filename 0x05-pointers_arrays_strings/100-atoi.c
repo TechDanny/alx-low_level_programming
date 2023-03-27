d8528d818815d420c9a6f794c1eb67e7b7a280a4
TechDanny <kokidanny15@gmail.com>
2023-03-27 15:33:50 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,21 +9,26 @@
 
 int _atoi(char *s)
 {
-	int sign = 1, res = 0, i = 0;
+	int sign;
+	unsigned int num;
+	char *temp;
 
-	while (s[i] != '\0')
+	temp = s;
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
-	if (s[i] == '-')
-	sign *= -1;
-
-	if (s[i] >= '0' && s[i] <= '9')
-		res = res * 10 + s[i] - '0';
-
-	if (res != 0 && !(s[i] >= '0' && s[i] <= '9'))
-	break;
-
-	i++;
+		if (*temp == '-')
+			sign *= -1;
+		temp++;
 	}
-
-	return (res * sign);
+	if (*temp != '\0')
+	{
+		do {
+			num = num * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
 }
+
