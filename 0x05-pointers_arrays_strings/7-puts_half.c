d3074a43fa2203f23434ce614141358306612df6
TechDanny <kokidanny15@gmail.com>
2023-03-27 13:19:48 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half -  prints half of a string
+ * @str:string
+ *
+ * return: returns void
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	int start_index;
+	int i;
+
+	if (length % 2 == 0)
+	{
+		start_index = length / 2;
+	}
+	else
+	{
+		start_index = (length - 1) / 2;
+	}
+
+	for (i = start_index; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
