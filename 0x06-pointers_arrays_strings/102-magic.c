7f3f9a0c7eceab4c05283a14254cf8b1224792d1
TechDanny <kokidanny15@gmail.com>
2023-03-30 16:54:29 -0700

    capitalize strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - entry point
  *
- * Return:returns 0
+ * return: always o
  */
 
 int main(void)
@@ -14,7 +14,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	*p = *(p - 2) + 96;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+	*(p + 5) = 98;
+  /* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
