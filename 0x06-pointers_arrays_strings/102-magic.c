f31f46413dd56bdf9053f71760c25468e235783d
TechDanny <kokidanny15@gmail.com>
2023-03-30 16:33:07 -0700

    magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return:returns 0
+ */
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	*p = *(p - 2) + 96;
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
