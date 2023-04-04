aba0320bc44204a83a202f83fb71930a98966ba3
TechDanny <kokidanny15@gmail.com>
2023-04-03 18:14:50 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include<stddef.h>
+#include<string.h>
 
 /**
  * _strstr -  locates a substring
@@ -10,4 +12,24 @@
 
 char *_strstr(char *haystack, char *needle)
 {
+	int haystack_len = strlen(haystack);
+	int needle_len = strlen(needle);
+	int x;
+	int y;
 
+	for (x = 0; x <= haystack_len - needle_len; x++)
+	{
+	for (y = 0; y < needle_len; y++)
+	{
+	if (haystack[x + y] != needle[y])
+	{
+	break;
+	}
+	}
+	if (y == needle_len)
+	{
+	return (&haystack[x]);
+	}
+	}
+	return (NULL);
+}
