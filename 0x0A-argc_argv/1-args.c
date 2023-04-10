ad9fa37e8a4033a3c823aaef57e1c659fdb71278
TechDanny <kokidanny15@gmail.com>
2023-04-10 14:25:07 -0700

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ * @argc:counts arguments
+ * @argv:array of strings
+ *
+ * Return:always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc -1);
+	
+	return (0);
+}
