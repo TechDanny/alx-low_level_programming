a0028f42ec718b7d40516f02754f65015ae967e1
TechDanny <kokidanny15@gmail.com>
2023-04-11 15:13:38 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,4 +5,31 @@
  * _strdup - contains a copy of string
  * @str:string
  *
- *
+ * Return:returns a pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *pstr;
+	int count;
+	int len;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	pstr = malloc(sizeof(char) * (len + 1));
+	if (pstr == NULL)
+	{
+		return (NULL);
+	}
+	for (count = 0; count < len; count++)
+	{
+		pstr[count] = str[count];
+	}
+	pstr[count] = '\0';
+	return (pstr);
+}
+
