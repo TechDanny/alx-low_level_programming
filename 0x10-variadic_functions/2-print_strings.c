02eb5c7be36f51f32cf3ca6233a3a4ffc48dfaa5
TechDanny <kokidanny15@gmail.com>
2023-04-20 12:51:24 -0700

    print numbers

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print strings
+ * @separator:seperates strings
+ * @n:number of elements
+ *
+ * Return:returns void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...);
+{
+
