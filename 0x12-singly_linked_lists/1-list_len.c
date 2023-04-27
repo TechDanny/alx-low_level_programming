cecd5e799568d5081461869f919357f17d38525e
TechDanny <kokidanny15@gmail.com>
2023-04-27 13:33:08 -0700

    list length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elem
+ * @h:pointer to first node
+ *
+ * Return:number
+ */
+
+size_t list_len(const list_t *h);
+{
+	int node_count;
+
+	while(h)
+	{
+		node_count++;
+		h = h->next;
+	}
+	return (node_count);
+}
