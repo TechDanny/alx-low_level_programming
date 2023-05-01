f6e00104b297027cdced87d24e554b44d13745d7
TechDanny <kokidanny15@gmail.com>
2023-05-01 04:21:42 -0700

    free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * free_listint - frees a list
- * @head - address to first node
+ * @head:address to first node
  *
  */
 
@@ -10,10 +10,9 @@ void free_listint(listint_t *head)
 {
 	listint_t *ptr;
 
-	ptr = head;
-
 	while (head != NULL)
 	{
+		ptr = head;
 		head = head->next;
 		free(ptr);
 	}
